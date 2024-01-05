3f097d296b0aab2f8f63879837477ffec40b8f28
cernoh <zoiny@outlook.com>
2024-01-05 00:58:56 +0000

    initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,12 @@
+local colorscheme = require "lazyvim.plugins.colorscheme"
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "xiyaowong/transparent.nvim"
+  }
+}
